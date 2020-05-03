48b9ddbe0c287c1c6216b350df3d542382ea63f2
Mykola Kolotylo <mykolakolotilo@gmail.com>
2020-05-03 13:42:31 +0300

    added array of words and writted test for him

--- src/GuessdWords/GuessedWords.js
+++ src/GuessdWords/GuessedWords.js
@@ -5,9 +5,32 @@ const GuessedWords = props => {
     let contents;
     if (props.guessedWords.length === 0) {
         contents = (
-            <span data-test="guess instructions" >
+            <span data-test="guess-instructions" >
                 Try to guess the secret word!
             </span>
+        )
+    } else {
+        const guessedWordsRow = props.guessedWords.map((word, i) => (
+            <tr  data-test="guessed-word" key={i}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordsRow}
+                    </tbody>
+                </table>
+            </div>
         )
     }
     return ( <div data-test="component-guessed-words">{contents}</div>)
--- src/GuessdWords/GuessedWords.test.js
+++ src/GuessdWords/GuessedWords.test.js
@@ -26,8 +26,33 @@ describe('if there are no words guessed', () => {
     });
 
     it('renders instructions to guess a word', () => {
-        const instructions = findByTestAttr(wrapper, 'guess instructions');
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
         expect(instructions.text().length).not.toBe(0);
     });
 });
-describe('if there are words guessed', () => {});
+describe('if there are words guessed', () => {
+    let wrapper;
+   const guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3},
+        {guessedWord: 'agile', letterMatchCount: 1},
+        {guessedWord: 'party', letterMatchCount: 5},
+    ];
+    beforeEach(() => {
+        wrapper = setup({guessedWords});
+    });
+
+    it('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+
+    it('renders "guessed words" section', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        expect(guessedWordsNode.length).toBe(1);
+    });
+
+    it('correct number of guessed words', () => {
+        const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+        expect(guessedWordsNodes.length).toBe(guessedWords.length);
+    });
+});
